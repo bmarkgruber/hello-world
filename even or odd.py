e2451984853d14618be33aade6a906e4e4dbad67
Brian M Gruber <60892507+bmarkgruber@users.noreply.github.com>
2020-10-30 14:36:42 -0500

    Add files via upload

---
+++ even or odd.py
@@ -0,0 +1,15 @@
+num = int(input('Pick a number: '))
+check = int(input('Is number divisible by '))
+
+#check if even or odd
+if num % 2 == 0:
+    print(num, 'is a even!')
+else:
+    print(num, 'is odd!')
+
+#check if num us a multiple of check
+mult_of_check = num % check
+if mult_of_check == 0:
+    print('Number is a multiple of ' + str(check))
+else:
+    print('Number is not a multiple of ' + str(check))
